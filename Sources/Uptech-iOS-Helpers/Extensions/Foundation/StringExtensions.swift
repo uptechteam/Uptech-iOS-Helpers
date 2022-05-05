636ef39ca473d14181ccc491a27411be6cc3b34a
Alexander Kulyk <a.kulyk97@gmail.com>
2022-05-05 16:28:04 +0300

    add NSAttributedString builder

---
+++ Sources/Uptech-iOS-Helpers/Extensions/Foundation/StringExtensions.swift
@@ -0,0 +1,83 @@
+//
+//  StringExtensions.swift
+//  
+//
+//  Created by Alexander Kulyk on 26.04.2022.
+//
+
+import Foundation
+
+// MARK: - NSAttributedString builder
+
+public extension String {
+    /// The wrapper for a text message and an underlying `URL` to which the user will navigate upon tap.
+    struct ClickableElement {
+        let text: String
+        let url: URL?
+
+        /// Initializes a new clickable text element with the provided copy and link.
+        /// - Parameters:
+        ///   - text: Clickable text element body.
+        ///   - url: The `URL` to which the user will navigate upon tap.
+        init(
+            text: String,
+            url: URL?
+        ) {
+            self.text = text
+            self.url = url
+        }
+    }
+
+    /// Builds an attributed string from format string by replacing **\*** occurrences with clickable and **\*\*** with bold text elements.
+    /// - Complexity: O(N) where N is a sum of clickable and bold elements.
+    /// - Warning: If the number of placeholders exceeds the corresponding elements count - extra placeholders will be displayed as asterisks in the resulting string. Excessive elements will not be displayed if there are fewer placeholders than elements.
+    /// - Parameters:
+    ///   - format: Describes the format of a resulting attributed string. It contains **\*** and **\*\*** placeholders for clickable and bold elements.
+    ///   - formatAttributes: Attributes applied to the whole **format** string.
+    ///   - clickableElements: Elements replacing **\*** occurrences in the **format** string.
+    ///   - clickableAttributes: Attributes applied to clickable elements. Values from **formatAttributes** will be used if no **clickableAttributes** values are provided.
+    ///   - boldElements: Elements replacing **\*\*** occurrences in the **format** string.
+    ///   - boldAttributes: Attributes applied to bold elements. Values from **formatAttributes** will be used if no **boldAttributes** values are provided.
+    /// - Returns: New constructed instance of mutable attributed string.
+    static func attributedString(
+        format: String,
+        formatAttributes: [NSAttributedString.Key: Any],
+        clickableElements: [ClickableElement] = [],
+        clickableAttributes: [NSAttributedString.Key: Any] = [:],
+        boldElements: [String] = [],
+        boldAttributes: [NSAttributedString.Key: Any] = [:]
+    ) -> NSMutableAttributedString {
+        let attributedFormat = NSMutableAttributedString(string: format, attributes: formatAttributes)
+
+        for element in boldElements {
+            attributedFormat.string.range(of: "**").map { foundRange in
+                let range = NSRange(foundRange, in: attributedFormat.string)
+                let attributedElement = NSAttributedString(
+                    string: element,
+                    attributes: boldAttributes.isEmpty ? formatAttributes : boldAttributes
+                )
+                attributedFormat.replaceCharacters(in: range, with: attributedElement)
+            }
+        }
+
+        for element in clickableElements {
+            attributedFormat.string.range(of: "*").map { foundRange in
+                let range = NSRange(foundRange, in: attributedFormat.string)
+                let linkAttributes = makeLinkAttributes(with: element.url, clickableAttributes: clickableAttributes)
+                let attributedElement = NSAttributedString(string: element.text, attributes: linkAttributes)
+                attributedFormat.replaceCharacters(in: range, with: attributedElement)
+            }
+        }
+
+        return attributedFormat
+    }
+
+    private static func makeLinkAttributes(
+        with url: URL?,
+        clickableAttributes: [NSAttributedString.Key: Any]
+    ) -> [NSAttributedString.Key: Any] {
+        var attributes = clickableAttributes
+        attributes[.link] = url
+        return attributes
+    }
+}
---
+++ Sources/Uptech-iOS-Helpers/Extensions/UIKit/UIViewExtensions.swift
@@ -0,0 +1,104 @@
+//
+//  UIViewExtensions.swift
+//  
+//
+//  Created by Sergey Kletsov on 25.03.2022.
+//
+
+import UIKit
+
+// MARK: - Subview addition
+
+public extension UIView {
+    /// Adds subview to self.
+    /// Sets subview's `translatesAutoresizingMaskIntoConstraints` to `false` and activates given constraints array.
+    /// - Parameters:
+    ///   - view: View to add as a subview and disable Autoresizing Mask.
+    ///   - constraints: Constraints array to activate after adding subview and disabling Autoresizing Mask.
+    func addSubview( _ view: UIView, withConstraints constraints: [NSLayoutConstraint]) {
+        addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        constraints.activate()
+    }
+
+    /// Adds subview to self.
+    /// Sets subview's `translatesAutoresizingMaskIntoConstraints` to `false` and adds constraints to selfs edges.
+    /// - Parameter view: View to add as a subview and disable Autoresizing Mask.
+    func addSubviewWithEdgeConstraints(_ view: UIView) {
+        addSubview(view, withConstraints: [
+            view.topAnchor.constraint(equalTo: topAnchor),
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    /// Adds subview to self.
+    /// Sets subview's `translatesAutoresizingMaskIntoConstraints` to `false` and adds constraints to selfs `layoutMarginsGuide`.
+    /// - Parameter view: View to add as a subview and disable Autoresizing Mask.
+    func addSubviewWithMarginEdgeConstraints(_ view: UIView) {
+        addSubview(view, withConstraints: [
+            view.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            view.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
+        ])
+    }
+
+    /// Inserts subview at a given index with given constraints.
+    /// Also will set subview's `translatesAutoresizingMaskIntoConstraints` to `false`.
+    /// - Parameters:
+    ///   - view: View to insert as a subview and disable Autoresizing Mask.
+    ///   - index: The index in the array of the subviews property at which to insert the view. Defaults to `.zero`.
+    ///   - constraints: Constraints array to activate after inserting subview and disabling Autoresizing Mask.
+    func insertSubview(_ view: UIView, at index: Int = .zero, withConstraints constraints: [NSLayoutConstraint] ) {
+        insertSubview(view, at: index)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        constraints.activate()
+    }
+
+    /// Inserts subview at a given index and adds constraints to selfs edges.
+    /// Also will set subview's `translatesAutoresizingMaskIntoConstraints` to `false`.
+    /// - Parameters:
+    ///   - view: View to insert as a subview and disable Autoresizing Mask.
+    ///   - index: The index in the array of the subviews property at which to insert the view. Defaults to `.zero`.
+    func insertSubviewWithEdgeConstraints(_ view: UIView, at index: Int = .zero) {
+        insertSubview(view, at: index, withConstraints: [
+            view.topAnchor.constraint(equalTo: topAnchor),
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    /// Inserts subview at a given index and adds constraints to selfs `layoutMarginsGuide`.
+    /// Also will set subview's `translatesAutoresizingMaskIntoConstraints` to `false`.
+    /// - Parameters:
+    ///   - view: View to insert as a subview and disable Autoresizing Mask.
+    ///   - index: The index in the array of the subviews property at which to insert the view. Defaults to `.zero`.
+    func insertSubviewWithMarginEdgeConstraints(_ view: UIView, at index: Int = .zero) {
+        insertSubview(view, at: index, withConstraints: [
+            view.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            view.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - Rounded corners
+
+public extension UIView {
+    /// Adds corner radius to given corners.
+    /// - Parameters:
+    ///   - radius: Corner radius to add.
+    ///   - corners: Corners to add corner radius to. Default value .all
+    func roundCornersContinuously(radius: CGFloat, corners: CACornerMask = .all) {
+        layer.maskedCorners = corners
+        layer.cornerRadius = radius
+
+        if #available(iOS 13.0, *) {
+            layer.cornerCurve = .continuous
+        }
+    }
+}
